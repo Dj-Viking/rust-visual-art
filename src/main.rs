5b201d3ef7a642392b9fdd1a6dea1c16655a29f1
dj-viking <anders.swedishviking@gmail.com>
2024-12-14 15:35:21 -0500

    print some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use console_engine::pixel;
+use console_engine::Color;
+use console_engine::KeyCode;
 fn main() {
-    println!("Hello, world!");
+
+    let mut engine = console_engine::ConsoleEngine::init(20, 20, 3).unwrap();
+
+    loop {
+        engine.wait_frame();
+        engine.clear_screen();
+
+        engine.print(0,0,
+            "press q to quit");
+
+        for i in 1..19 {
+            for j in 1..19 {
+                engine.set_pxl(
+                    i,j,
+                    pixel::pxl_fg(
+                        '0',
+                        Color::Yellow));
+            }
+        }
+
+        if engine.is_key_pressed(
+            KeyCode::Char('q')) 
+        {
+            break;
+        }
+        engine.draw();
+    }
 }
