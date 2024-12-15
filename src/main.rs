5d8be60beb22ea388de87d27bc78c85890f86990
dj-viking <anders.swedishviking@gmail.com>
2024-12-14 19:32:53 -0500

    full screen in default new terminal window character width and height

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,49 @@ use console_engine::Color;
 use console_engine::KeyCode;
 use console_engine::screen::Screen;
 
+const FPS: u32 = 60;
+
 fn draw_stuff() {
     let mut engine = console_engine::ConsoleEngine::init(
         // dimensions
-        21, 21, 
-        //fps
-        60
+        136, 40,
+        FPS
     ).unwrap();
 
     let mut thing: i32 = 0;
+
     loop {
         thing += 1;
         engine.wait_frame();
         engine.clear_screen();
 
-        engine.print(1,1,
-            "press q to quit");
-
-        for i in 2..20 
+        for i in 0..136 
         {
-            for j in 2..20 
+            for j in 2..40 
             {
                 engine.set_pxl(
                     i,j,
-                    pixel::pxl_bg(' ', Color::Black));
+                    pixel::pxl_bg(' ', Color::Green));
             }
         }
         // draw * around perimeter of 'screen'
-        engine.rect(
-            2,2, 
-            20,20,
-            pixel::pxl('*'));
-        engine.fill_circle(
-            7,8,
-            2,
-            pixel::pxl_bg(' ', Color::Green)
-        );
+        // engine.rect(
+        //     2,2, 
+        //     20,20,
+        //     pixel::pxl('*'));
+        // engine.fill_circle(
+        //     7,8,
+        //     2,
+        //     pixel::pxl_bg(' ', Color::Green)
+        // );
 
         engine.print(
             0,0,
             format!("delta counter: {}",
                 thing.to_string()).as_str());
+
+        engine.print(1,1,
+            "press q to quit");
 
         if engine.is_key_pressed(
             KeyCode::Char('q')) 
