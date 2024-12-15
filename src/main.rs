5c533577f81e167ffdff189a2f382cf93afeb25c
dj-viking <anders.swedishviking@gmail.com>
2024-12-15 11:46:30 -0500

    better

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use console_engine::pixel;
-use console_engine::Color;
-use console_engine::KeyCode;
-use console_engine::screen::Screen;
+use console_engine::{pixel, Color, KeyCode, ConsoleEngine};
 
 // todo: somehow determine width based on current size of terminal character (preference?)
-const WIDTH: u32 = 136;
-const HEIGHT: u32 = 40;
+//
+const WIDTH: u32 = 80;
+const HEIGHT: u32 = 28;
 
-const FPS: u32 = 60;
+const FPS: u32 = 10;
 
-fn get_color_from_coord(coord: (i32, i32)) -> Color {
+fn handle_keypress_quit(engine: &ConsoleEngine) -> bool {
+    let mut should_quit = false;
+    if engine.is_key_pressed(KeyCode::Char('q')) {
+        should_quit = true;
+    }
+    should_quit
+}
+fn get_color_from_coord(coord: (i32, i32), delta: i32) -> Color {
     let mut ret = Color::Black;
-    if coord.0 % 2 == 0 && coord.1 % 3 == 0 {
+    if coord.0 % 2 == 0 && coord.1 % delta == 0 {
         ret = Color::Green;
     }
-    else if coord.0 % 2 == 0 && coord.1 % 3 == 1 {
+    else if coord.0 % delta == 0 && coord.1 % 3 == 1 {
         ret = Color::Cyan;
     }
-    else if coord.0 % 4 == 0 && coord.1 % 2 == 0 {
+    else if coord.0 % delta / 6 == 1 && coord.1 / delta * 2 == 0 {
         ret = Color::Blue;
     }
     ret
@@ -25,16 +30,17 @@ fn get_color_from_coord(coord: (i32, i32)) -> Color {
 
 fn draw_stuff() {
 
-    let mut engine = console_engine::ConsoleEngine::init(
+    let mut engine = ConsoleEngine::init(
         // dimensions
         WIDTH, HEIGHT,
         FPS
     ).unwrap();
 
-    let mut thing: i32 = 0;
+    let mut delta: i32 = 0;
 
     loop {
-        thing += 1;
+        delta += 1;
+        if delta > (HEIGHT as i32) / 2 { delta = 1 }
         engine.wait_frame();
         engine.clear_screen();
 
@@ -48,36 +54,24 @@ fn draw_stuff() {
                     pixel::pxl_bg(' ', 
                         get_color_from_coord(
                             (i as i32,
-                            j as i32)
+                            j as i32),
+                            delta
                         )
                     )
                 );
             }
         }
-        // draw * around perimeter of 'screen'
-        // engine.rect(
-        //     2,2, 
-        //     20,20,
-        //     pixel::pxl('*'));
-        // engine.fill_circle(
-        //     7,8,
-        //     2,
-        //     pixel::pxl_bg(' ', Color::Red)
-        // );
 
         engine.print(
             0,0,
             format!("delta counter: {}",
-                thing.to_string()).as_str());
+                delta.to_string()).as_str());
 
         engine.print(1,1,
             "press q to quit");
 
-        if engine.is_key_pressed(
-            KeyCode::Char('q')) 
-        {
-            break;
-        }
+        if handle_keypress_quit(&engine) { break; }
+
         engine.draw();
     }
 }
