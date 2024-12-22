657c1abf2ea2cc7736584365cdeb21f58f81d183
slbsh <crafter.antek@gmail.com>
2024-12-22 23:36:41 +0100

    unretardify

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,41 @@
-use console_engine::{pixel, Color, KeyCode, ConsoleEngine};
+use std::io::Write;
 
-// todo: somehow determine width based on current size of terminal character (preference?)
-//
-const WIDTH: u32 = 40;
-const HEIGHT: u32 = 30;
-
-const FPS: u32 = 10;
-
-fn handle_keypress_quit(engine: &ConsoleEngine) -> bool {
-    let mut should_quit = false;
-    if engine.is_key_pressed(KeyCode::Char('q')) {
-        should_quit = true;
-    }
-    should_quit
-}
-
-fn handle_keypress_interactive(engine: &ConsoleEngine) -> i32 {
-    if engine.is_key_pressed(KeyCode::Char('9')) {
-        return 1;
-    }
-    if engine.is_key_pressed(KeyCode::Char('0')) {
-        return -1;
-    }
-    0
-}
-
-fn v2_pattern(coord: (i32, i32), delta: i32, adjust: i32) -> () {
-    // 32 / (countRef / (row + 1)) +
-    // (col + 1) / ((row + 1) / (col + 1) - Number(animVarCoeff) / countRef) +
-    // countRef * ((col + 1) * 0.05);
-}
-// chance to overflow with multiply :( maybe have to use 64 
-fn spiral_pattern(coord: (i32, i32), delta: i32, adjust: i32) -> Color {
-    // THIS IS THE SPIRAL PATTERN! add delta to animate overtime!
-    //const num = row * 8 * col * Number(animVarCoeff) + countRef;
-    //
-    let col = coord.1;
-    let row = coord.0;
-    let blue: u8 = (col * (delta * adjust) * row * (adjust * 2)) as u8;
-    let red: u8 = (20) as u8;
-    let green: u8 = (10) as u8;
-
-    let rgb_vals = (red,green,blue);
-    let rgb = Color::from(rgb_vals);
-    rgb
-}
-
-fn draw_stuff() {
-
-    let mut engine = ConsoleEngine::init(
-        // dimensions
-        WIDTH, HEIGHT,
-        FPS
-    ).unwrap();
-
-    let mut delta: i32 = 0;
-    let mut adjust: i32 = 0;
-
-    loop {
-        delta += 1;
-
-        // reset delta after reaches half height of screen
-        if delta > 255 { delta = 1 }
-
-        engine.wait_frame();
-        engine.clear_screen();
-
-        // collection audio samples snapshot here?
-
-        for col in 0..WIDTH 
-        {
-            for row in 2..HEIGHT 
-            {
-                engine.set_pxl(
-                    col as i32, row as i32,
-                    pixel::pxl_bg(' ', 
-                        spiral_pattern(
-                            (col as i32,
-                            row as i32),
-                            delta,
-                            adjust
-                        )
-                    )
-                );
-            }
-        }
-
-        engine.print(
-            0,0,
-            format!("delta counter: {}",
-                delta.to_string()).as_str());
-
-        engine.print(1,1,
-            "press q to quit");
-
-        engine.print(28,1,
-            format!("adjust runtime value: {}", adjust).as_str());
-
-        engine.print(28,0,
-            format!("color at runtime: {:?}", 
-                spiral_pattern((20, 20), delta, adjust)
-            ).as_str());
-
-        if handle_keypress_quit(&engine) { break; }
-
-        adjust += handle_keypress_interactive(&engine);
-
-        engine.draw();
-    }
-}
-
-fn main1() {
-    let data: u8 = 255;
-    println!("hex data => {} => {:x?}", data, data);
-}
+const CLEAR: &[u8] = b"\x1b[2J\x1b[1;1H";
+const RESET: &str = "\x1b[0m";
 
 fn main() {
+	let mut out = std::io::stdout();	
+	out.write_all(CLEAR).unwrap();
 
-    draw_stuff();
+	const FPS: u64 = 10;
 
+	const WIDTH: usize = 180;
+	const HEIGHT: usize = 40;
+
+	let spiral_pattern = |col: usize, row: usize, frame: usize, adjust: usize|
+		(20, 10, (col * (frame * adjust) * row * (adjust * 2)) as u8);
+
+	let color_to_escape = |color: (u8, u8, u8)|
+		format!("\x1b[38;2;{};{};{}m", color.0, color.1, color.2);
+
+	let mut ptime = std::time::Instant::now();
+	let mut frame = 0;
+
+	loop {
+		frame += 1;
+		std::thread::sleep(std::time::Duration::from_millis(1000 / FPS - ptime.elapsed().as_millis() as u64));
+		// let delta = ptime.elapsed().as_millis(); // ACTUAL Δt
+		ptime = std::time::Instant::now();
+
+		out.write_all(CLEAR).unwrap();
+		(0..HEIGHT).for_each(|c| {
+			(0..WIDTH).for_each(|r| { 
+				out.write_all(format!("{}█{RESET}", 
+					color_to_escape(spiral_pattern(c, r, frame, 1))).as_bytes()).unwrap(); 
+			});
+			out.write_all(b"\n").unwrap();
+		});
+
+		out.flush().unwrap();
+	}
 }
