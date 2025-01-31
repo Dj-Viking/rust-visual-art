49f7cebeb7f5ad2a1bae92fc2461ef72fb463d5e
dj-viking <anders.swedishviking@gmail.com>
2025-01-30 20:21:14 -0500

    using keys for controls instead of being forced to use midi if it's not connected

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ struct MutState {
 	is_backwards:      bool,
 	is_reset:          bool,
 	current_intensity: u8,
-	time_dialiation:   u8,
+	time_dialation:   u8,
 	func:              ActiveFunc,
 }
 
@@ -43,6 +43,7 @@ struct DConfig {
 const CONF_FILE: &str = "config.toml";
 
 fn main() {
+	
 	if std::env::args().skip(1).any(|a| a == "list") {
 		let pm_ctx = pm::PortMidi::new().unwrap();
 		let devices = pm_ctx.devices().unwrap();
@@ -56,82 +57,89 @@ fn main() {
 		let pm_ctx = pm::PortMidi::new().unwrap();
 		let devices = pm_ctx.devices().unwrap();
 
-		let (cfg, dev) = {
-			let mut config: HashMap<String, DConfig> = 
-				toml::from_str(&std::fs::read_to_string(CONF_FILE).unwrap()).unwrap_or_else(|e| {
-					eprintln!("Error reading config file: {e}");
-					std::process::exit(1);
-				});
+		if !std::env::args().skip(1).any(|a| a == "keys") {
 
-			let dev = devices.into_iter()
-				.find(|d| d.direction() == pm::Direction::Input && config.keys().any(|n| n == d.name()))
-				.unwrap_or_else(|| {
-					eprintln!("No device defined in config found");
-					std::process::exit(1);
-				});
+			println!("[INFO]: not running in keyboard mode using midi controller");
 
-			(unsafe { config.remove(dev.name()).unwrap_unchecked() }, dev)
-		};
+			let (cfg, dev) = {
+				let mut config: HashMap<String, DConfig> = 
+					toml::from_str(&std::fs::read_to_string(CONF_FILE).unwrap()).unwrap_or_else(|e| {
+						eprintln!("Error reading config file: {e}");
+						std::process::exit(1);
+					});
 
-		let ms_ = ms.clone();
-		std::thread::spawn(move || {
-			let mut in_port = pm_ctx.input_port(dev, 256).unwrap();
+				let dev = devices.into_iter()
+					.find(|d| d.direction() == pm::Direction::Input && config.keys().any(|n| n == d.name()))
+					.unwrap_or_else(|| {
+						eprintln!("No device defined in config found");
+						std::process::exit(1);
+					});
 
-			loop {
-				static mut BACKOFF: u8 = 0;
-				// TODO: listen flag
+				(unsafe { config.remove(dev.name()).unwrap_unchecked() }, dev)
+			};
 
-				let Ok(Some(m)) = in_port.read() else {
-					std::hint::spin_loop();
-					std::thread::sleep(std::time::Duration::from_millis(unsafe { BACKOFF * 10 } as u64));
-					unsafe { BACKOFF += 1; }
-					unsafe { BACKOFF %= 10; }
-					continue;
-				};
+			let ms_ = ms.clone();
+			std::thread::spawn(move || {
+				let mut in_port = pm_ctx.input_port(dev, 256).unwrap();
 
-				let channel   = m.message.data1;
-				let intensity = m.message.data2;
-				
-				println!("chan {} - intensity {}", channel, intensity);
+				loop {
+					static mut BACKOFF: u8 = 0;
+					// TODO: listen flag
 
-				let mut ms = ms_.lock().unwrap();
+					let Ok(Some(m)) = in_port.read() else {
+						std::hint::spin_loop();
+						std::thread::sleep(std::time::Duration::from_millis(unsafe { BACKOFF * 10 } as u64));
+						unsafe { BACKOFF += 1; }
+						unsafe { BACKOFF %= 10; }
+						continue;
+					};
 
-				if channel == cfg.intensity {
-					ms.current_intensity = intensity;
+					let channel   = m.message.data1;
+					let intensity = m.message.data2;
+					
+					println!("chan {} - intensity {}", channel, intensity);
+
+					let mut ms = ms_.lock().unwrap();
+
+					if channel == cfg.intensity {
+						ms.current_intensity = intensity;
+					}
+
+					if channel == cfg.time_dialation {
+						ms.time_dialation = intensity;
+					}
+
+					if channel == cfg.spiral && intensity > 0 {
+						ms.func = ActiveFunc::Spiral;
+					}
+
+					if channel == cfg.v2 && intensity > 0 {
+						ms.func = ActiveFunc::V2;
+					}
+
+					if channel == cfg.waves && intensity > 0 {
+						ms.func = ActiveFunc::Waves;
+					}
+
+					if channel == cfg.solid && intensity > 0 {
+						ms.func = ActiveFunc::Solid;
+					}
+
+					ms.is_reset = channel == cfg.reset && intensity > 0;
+
+					if channel == cfg.backwards && intensity > 0 {
+						ms.is_backwards = !ms.is_backwards;
+					}
+
+					unsafe { BACKOFF = 0; }
 				}
-
-				if channel == cfg.time_dialation {
-					ms.time_dialiation = intensity;
-				}
-
-				if channel == cfg.spiral && intensity > 0 {
-					ms.func = ActiveFunc::Spiral;
-				}
-
-				if channel == cfg.v2 && intensity > 0 {
-					ms.func = ActiveFunc::V2;
-				}
-
-				if channel == cfg.waves && intensity > 0 {
-					ms.func = ActiveFunc::Waves;
-				}
-
-				if channel == cfg.solid && intensity > 0 {
-					ms.func = ActiveFunc::Solid;
-				}
-
-				ms.is_reset = channel == cfg.reset && intensity > 0;
-
-				if channel == cfg.backwards && intensity > 0 {
-					ms.is_backwards = !ms.is_backwards;
-				}
-
-				unsafe { BACKOFF = 0; }
-			}
-		});
+			});
+		}
 
 		a.new_window()
 			.view(update)
+			.key_pressed(key_pressed)
+			.key_released(key_released)
 			.build().unwrap(); 
 
 		State {
@@ -147,6 +155,45 @@ fn main() {
 
 	nannou::app(init).run();
 }
+fn key_released(app: &App, s: &mut State, key: Key) {
+	let mut ms = s.ms.lock().unwrap();
+	match key {
+		Key::R => {
+			ms.is_reset = false;
+		},
+		_ => ()
+	}
+}
+
+fn key_pressed(app: &App, s: &mut State, key: Key) {
+	let mut ms = s.ms.lock().unwrap();
+	match key {
+		Key::R => {
+			ms.is_reset = true;
+		},
+		Key::S => ms.func = ActiveFunc::Spiral,
+		Key::O => ms.func = ActiveFunc::Solid,
+		Key::V => ms.func = ActiveFunc::V2,
+		Key::Up => {
+			if ms.current_intensity == 255 { return; } 
+			ms.current_intensity += 1;
+		},
+		Key::Down => {
+			if ms.current_intensity == 0 { return; }
+			ms.current_intensity -= 1;
+		},
+		Key::Right => {
+			if ms.time_dialation == 255 { return; } 
+			ms.time_dialation += 1;
+		},
+		Key::Left => {
+			if ms.time_dialation == 0 { return; }
+			ms.time_dialation -= 1;
+		},
+		_ => (),
+	}
+}
+
 
 fn update(app: &App, s: &State, frame: Frame) {
 	let draw = app.draw();
@@ -180,7 +227,7 @@ fn update(app: &App, s: &State, frame: Frame) {
 		if ms.is_reset { unsafe { TIME = 0.0; } } 
 		
 		let t = unsafe { TIME } /
-			(time_divisor + 100000.0 * (ms.time_dialiation as f32 / 10.0))
+			(time_divisor + 100000.0 * (ms.time_dialation as f32 / 10.0))
 			+ ms.current_intensity as f32 / 100.0;
 
 		let hue = s.funcs[ms.func as u8 as usize](r.y(), r.x(), t);
