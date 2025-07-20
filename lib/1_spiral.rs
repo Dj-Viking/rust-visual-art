f17f9aa73e71f7fe21a65cb5cb5cc1b63d28d658
Dj-Viking <anders.swedishviking@gmail.com>
2025-07-20 15:52:40 -0400

    fix

--- lib/1_spiral.rs
+++ lib/1_spiral.rs
@@ -3,5 +3,5 @@ pub static LUM_MOD: f32 = 100.0;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn transform(x: f32, y: f32, t: f32) -> f32 {
-    (y * 3.0) * (x * 1.0) * t * 1.0
+    (y * 1.0) * (x * 1.0) * t * 1.0
 }
