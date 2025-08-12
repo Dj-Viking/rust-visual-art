0ba189eb72dac95681ef49e0d8f2e96efc699592
Dj-Viking <anders.swedishviking@gmail.com>
2025-08-12 00:24:44 -0400

    better error

--- src/main.rs
+++ src/main.rs
@@ -191,7 +191,7 @@ fn main() {
 		let mut midi_ccs = vec![];
 		let midi_result = match pm_ctx {
 			Ok(ref ctx) => { midi::Midi::new(&ctx) },
-			Err(e)      => { Err(format!("could not get midi result {:?}", e).into()) }
+			Err(e)      => { Err(format!("could not use pm ctx to create our midi helper structure {:?}", e).into()) }
 		};
 		match pm_ctx {
 			Ok(ref ctx) => match utils::get_midi_ccs(&ctx) {
