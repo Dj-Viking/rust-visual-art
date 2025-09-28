66fa4a11d6453465e0a52cd4d54e444d8b4547d4
slbsh <crafter.antek@gmail.com>
2025-09-29 01:18:53 +0200

    chore: fix clippy warns in build.rs

--- build.rs
+++ build.rs
@@ -1,37 +1,35 @@
+use std::path::PathBuf;
+
+const LIBS_PATH: &str = "target/libs";
+
 fn main() {
-
-	const LIBS_PATH: &str = "target/libs";
-
-	if let Ok(entries) = std::fs::read_dir(LIBS_PATH) {
-		for entry in entries {
-			if let Ok(entry) = entry {
-				println!("remove {:?}", entry.path());
-				std::fs::remove_file(entry.path()).unwrap();
-			}
-		}
-	}
+	std::fs::read_dir(LIBS_PATH).unwrap()
+		.filter_map(|entry| {
+			let path = entry.ok()?.path();
+			path.is_file().then_some(path)
+		})
+		.for_each(|path| std::fs::remove_file(path).unwrap());
 	
 	std::fs::read_dir("lib").unwrap()
 		.filter_map(Result::ok)
 		.filter(|entry| entry.file_type().unwrap().is_file())
 		.for_each(|entry| handle_compile_dylib(&entry));
 }
-fn handle_compile_dylib(entry: &std::fs::DirEntry) -> () {
+
+fn handle_compile_dylib(entry: &std::fs::DirEntry) {
+	let _ = std::fs::create_dir(LIBS_PATH);
+
 	let path = entry.path();
+
+	let libs_path = PathBuf::from(LIBS_PATH).join(path.file_name().unwrap());
 	
 	let status = std::process::Command::new("rustc")
-		.args(["--crate-type=dylib", "-o", 
-			&("target/libs/".to_string() + path.file_stem().unwrap().to_str().unwrap()),
-			path.to_str().unwrap()])
+		.args(["--crate-type=dylib", "-o"])
+		.args([&libs_path, &path])
 		.output()
 		.unwrap();
 
-	if !status.status.success() {
-		std::fs::create_dir("target/libs/").unwrap();
-		return handle_compile_dylib(entry);
-	} else {
-		println!("{}", std::str::from_utf8(&status.stdout).unwrap());
-		println!("{}", std::str::from_utf8(&status.stderr).unwrap());
-		assert!(status.status.success());
-	}
+	println!("{}", std::str::from_utf8(&status.stdout).unwrap());
+	println!("{}", std::str::from_utf8(&status.stderr).unwrap());
+	assert!(status.status.success());
 }
